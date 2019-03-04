0e7f6ff31d78cde796a80ea20eef9b3146f21e6e
Aleksandr <aleksandr@aleksandr.ru>
2019-03-04 11:48:34 +0300

    inn expore config

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { SERVER_PORT, DADATA_BASEURL, DADATA_TOKEN, REDIS_OPTIONS_URL, REDIS_EXPIRE } = process.env;
+const { SERVER_PORT, DADATA_BASEURL, DADATA_TOKEN, REDIS_OPTIONS_URL, REDIS_EXPIRE, REDIS_EXPIRE_INN } = process.env;
 const defaultExpire = 60;
 
 module.exports = {
@@ -17,11 +17,11 @@ module.exports = {
             url: REDIS_OPTIONS_URL || 'redis://127.0.0.1:6379', 
             // [redis[s]:]//[[user][:password@]][host][:port][/db-number]
         },
-        expire: REDIS_EXPIRE || defaultExpire
+        expire: REDIS_EXPIRE || 60
     },
     urlMap: {
         '/suggestions/api/4_1/rs/findById/party': {
-            expire: (REDIS_EXPIRE || defaultExpire) * 60 * 24 * 30, // month
+            expire: REDIS_EXPIRE_INN || (60 * 60 * 24 * 30), // month
             additionalKey: function(data) { return `inn:${data.query}`; }
         }
     }
